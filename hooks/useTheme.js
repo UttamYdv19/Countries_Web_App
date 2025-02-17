f20621b510b5e36a24bf1a826e22c9e904b14800
UttamYdv19 <uttamyadav1912@gmail.com>
2025-02-17 21:51:17 +0530

    Countries_Project

--- hooks/useTheme.js
+++ hooks/useTheme.js
@@ -1,4 +1,4 @@
 import { useContext } from "react"
 import { ThemeContext } from "../Contexts/ThemeContext"
 
-export default useTheme = () =>useContext(ThemeContext)
+export const useTheme = () =>useContext(ThemeContext)
